1200383e78fa51ba169a71867386cadfbdac7670
Eric Dahlgren <45627457+ericdahhl@users.noreply.github.com>
2025-09-14 21:24:17 +0200

    Fix symlinking for neovim

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -6,5 +6,8 @@
   };
 
   # Symlink your repo's nvim/ into ~/.config/nvim
-  xdg.configFile."nvim".source = ../../dotfiles/nvim;
+  xdg.configFile."nvim/init.lua".source = ../../dotfiles/nvim/init.lua;
+  xdg.configFile."nvim/lua".source = ../../dotfiles/nvim/lua;
+  xdg.configFile."nvim/neovim.yml".source = ../../dotfiles/nvim/neovim.yml;
+  xdg.configFile."nvim/selene.toml".source = ../../dotfiles/nvim/selene.toml;
 }
